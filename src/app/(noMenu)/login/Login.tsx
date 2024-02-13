5d5d955d38b8fd9c5178f2b4d5c71b7507b241f7
Krekinha <krekmg@gmail.com>
2024-02-13 19:23:15 -0300

    alt-19:22

--- src/app/(noMenu)/login/Login.tsx
+++ src/app/(noMenu)/login/Login.tsx
@@ -1,36 +1,37 @@
 "use client";
-import * as Yup from "yup";
-import { Formik, Form, Field } from "formik";
-import { ILogin } from "@/utils/types";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-const initialValues = {
-  email: "",
-  senha: "",
-};
+type ILogin = z.infer<typeof LoginSchema>;
 
-const LoginSchema = Yup.object().shape({
-  email: Yup.string().email("Email inválido").required("Campo obrigatório"),
-  senha: Yup.string()
-    .min(8, "Senha deve ter pelo menos 8 caracteres")
-    .required("Campo obrigatório"),
+const LoginSchema = z.object({
+  email: z
+    .string({ required_error: "Campo obrigatório" })
+    .email("Email inválido"),
+  senha: z.string().min(8, "Senha deve ter pelo menos 8 caracteres"),
 });
 
 export default function Login() {
   const [message, setMessage] = useState({ text: "" });
   const router = useRouter();
+  const { register, handleSubmit, formState } = useForm<ILogin>({
+    resolver: zodResolver(LoginSchema),
+  });
+  const { errors } = formState;
 
-  const handleSubmit = async (dados: ILogin) => {
+  async function login(dados: ILogin) {
     const resp = await signIn("credentials", {
       redirect: false,
       email: dados.email,
       senha: dados.senha,
     });
 
-    console.log(resp);
+    console.log(dados);
 
     if (resp!.ok == true) {
       console.log(resp);
@@ -39,13 +40,13 @@ export default function Login() {
       setMessage({ text: resp?.error || "erro?" });
       console.log(resp);
     }
-  };
+  }
 
   return (
     <>
       <div>
         <div className="flex-col md:flex-row justify-between flex gap-4 items-start mx-4 py-12">
-          <div className="mx-auto dark">
+          <div className="mx-auto">
             <div className="flex gap-x-2">
               <Image
                 src="/images/logo-aya.png"
@@ -66,101 +67,97 @@ export default function Login() {
                   Faça login em sua conta
                 </div>
                 <div className="mt-8">
-                  <Formik
-                    initialValues={initialValues}
-                    validationSchema={LoginSchema}
-                    onSubmit={async (values: ILogin, actions) => {
-                      handleSubmit(values);
-                    }}
-                  >
-                    {(props) => (
-                      <Form onSubmit={props.handleSubmit}>
-                        <div className="flex flex-col mb-2">
-                          <div className="flex relative ">
-                            <div>
-                              <div className="flex relative">
-                                <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
-                                  <svg
-                                    width="15"
-                                    height="15"
-                                    fill="currentColor"
-                                    viewBox="0 0 1792 1792"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                  >
-                                    <path d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z"></path>
-                                  </svg>
-                                </span>
-                                <Field
-                                  type="email"
-                                  name="email"
-                                  value={props.values.email}
-                                  className="rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                                  placeholder="Seu email"
-                                />
-                              </div>
-                              {props.touched.email && props.errors.email && (
-                                <span className="text-[0.75rem] text-red-500">
-                                  {props.errors.email}
-                                </span>
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-col mb-6">
-                          <div className="flex relative ">
-                            <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
-                              <svg
-                                width="15"
-                                height="15"
-                                fill="currentColor"
-                                viewBox="0 0 1792 1792"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z"></path>
-                              </svg>
-                            </span>
-                            <Field
-                              type="password"
-                              name="senha"
-                              value={props.values.senha}
-                              className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                              placeholder="Sua senha"
-                            />
-                          </div>
+                  {
+                    <form onSubmit={handleSubmit(login)}>
+                      <div className="flex flex-col mb-2">
+                        <div className="flex relative ">
                           <div>
-                            {props.touched.senha && props.errors.senha && (
-                              <span className="text-[0.75rem] text-red-500">
-                                {props.errors.senha}
+                            <div className="flex relative">
+                              <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-sky-600 shadow-sm text-sm">
+                                <svg
+                                  width="15"
+                                  height="15"
+                                  fill="currentColor"
+                                  viewBox="0 0 1792 1792"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path d="M1792 710v794q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-794q44 49 101 87 362 246 497 345 57 42 92.5 65.5t94.5 48 110 24.5h2q51 0 110-24.5t94.5-48 92.5-65.5q170-123 498-345 57-39 100-87zm0-294q0 79-49 151t-122 123q-376 261-468 325-10 7-42.5 30.5t-54 38-52 32.5-57.5 27-50 9h-2q-23 0-50-9t-57.5-27-52-32.5-54-38-42.5-30.5q-91-64-262-182.5t-205-142.5q-62-42-117-115.5t-55-136.5q0-78 41.5-130t118.5-52h1472q65 0 112.5 47t47.5 113z"></path>
+                                </svg>
                               </span>
-                            )}
-                          </div>
-                        </div>
-                        <div className="flex items-center mb-6 -mt-4">
-                          <div className="flex ml-auto">
-                            <a
-                              href="#"
-                              className="inline-flex text-xs underline text-gray-800 sm:text-sm hover:text-gray-700"
-                            >
-                              Esqueceu sua senha?
-                            </a>
-                          </div>
-                        </div>
-                        <div className="flex-coç w-full">
-                          <button
-                            type="submit"
-                            className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
-                          >
-                            Login
-                          </button>
-                          <div className="flex justify-center mt-1">
+                              <input
+                                type="email"
+                                className="rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                                placeholder="Email"
+                                {...register("email")}
+                              />
+                            </div>
                             <span className="text-[0.75rem] text-red-500">
-                              {message.text}
+                              {errors.email?.message}
                             </span>
+                            {/*props.touched.email && props.errors.email && (
+                              <span className="text-[0.75rem] text-red-500">
+                                {props.errors.email}
+                              </span>
+                            )*/}
                           </div>
                         </div>
-                      </Form>
-                    )}
-                  </Formik>
+                      </div>
+                      <div className="flex flex-col mb-6">
+                        <div className="flex relative ">
+                          <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-sky-600 shadow-sm text-sm">
+                            <svg
+                              width="15"
+                              height="15"
+                              fill="currentColor"
+                              viewBox="0 0 1792 1792"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z"></path>
+                            </svg>
+                          </span>
+                          <input
+                            type="password"
+                            className=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                            placeholder="Senha"
+                            {...register("senha")}
+                          />
+                        </div>
+                        <div>
+                        <span className="text-[0.75rem] text-red-500">
+                              {errors.senha?.message}
+                            </span>
+                          {/*props.touched.senha && props.errors.senha && (
+                            <span className="text-[0.75rem] text-red-500">
+                              {props.errors.senha}
+                            </span>
+                          )*/}
+                        </div>
+                      </div>
+                      <div className="flex items-center mb-6 -mt-4">
+                        <div className="flex ml-auto">
+                          <a
+                            href="#"
+                            className="inline-flex text-xs underline text-gray-800 sm:text-sm hover:text-gray-700"
+                          >
+                            Esqueceu sua senha?
+                          </a>
+                        </div>
+                      </div>
+                      <div className="flex-coç w-full">
+                        <button
+                          type="submit"
+                          className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                        >
+                          Login
+                        </button>
+                        <div className="flex justify-center mt-1">
+                          <span className="text-[0.75rem] text-red-500">
+                            {message.text}
+                          </span>
+                        </div>
+                      </div>
+                    </form>
+                  }
                 </div>
               </div>
             </div>
