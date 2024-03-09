7480c115058dc894758f45944ce5fac3b1fa6e7a
Krekinha <krekmg@gmail.com>
2024-03-09 04:21:35 -0300

    04:21

--- src/app/(menu-side)/teste/sidebar.tsx
+++ src/app/(menu-side)/teste/sidebar.tsx
@@ -1,20 +1,17 @@
 import classNames from "classnames";
 import { Sidebar as FlowbiteSidebar } from "flowbite-react";
 import type { FC, PropsWithChildren } from "react";
-import { useSidebarContext } from "./SidebarContext";
 
 const Sidebar: FC<PropsWithChildren<Record<string, unknown>>> = function ({
   children,
 }) {
-  const { isOpenOnSmallScreens: isSidebarOpenOnSmallScreens } =
-    useSidebarContext();
 
   return (
     <div
       className={classNames(
         "fixed overflow-auto top-0 h-screen z-10 lg:sticky lg:!block",
         {
-          hidden: !isSidebarOpenOnSmallScreens,
+          //hidden: !isSidebarOpenOnSmallScreens,
         }
       )}
     >
