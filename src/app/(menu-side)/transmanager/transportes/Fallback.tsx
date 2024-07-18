706aca59e9d92e0905289a1932ba3ea271f3fa53
Krekinha <krekmg@gmail.com>
2024-07-18 20:17:21 -0300

    20:17

--- src/app/(menu-side)/transmanager/transportes/Fallback.tsx
+++ src/app/(menu-side)/transmanager/transportes/Fallback.tsx
@@ -16,15 +16,15 @@ interface ErrorCustom {
 export function FallbackFetch({ error, resetErrorBoundary }: FallbackFetchProps) {
     // Call resetErrorBoundary() to reset the error boundary and retry the render.
 
-    const err: ErrorCustom = JSON.parse(error.message)
-    console.log(err);
+    //const err: ErrorCustom = JSON.parse(error.message)
+    console.log(error);
 
   
     return (
       <div role="alert">
-        <pre style={{ color: "red" }}>{err.message}</pre>
-        <pre style={{ color: "red" }}>Code: {err.statusCode}</pre>
-        <pre style={{ color: "red" }}>Response: {err.statusText}</pre>
+        <pre style={{ color: "red" }}>{error.message}</pre>
+        {/* <pre style={{ color: "red" }}>Code: {err.statusCode}</pre>
+        <pre style={{ color: "red" }}>Response: {err.statusText}</pre> */}
       </div>
     );
   }
--- src/utils/apiErrors.ts
+++ src/utils/apiErrors.ts
@@ -1,13 +1,9 @@
 
 export default class FetchFailedError extends Error {
-    statusCode?: number;
-    statusText?: string;
   
-    constructor(message?: string, statusCode?: number, statusText?: string) {
+    constructor(message?: string) {
     //   super(`${message} | Code: ${statusCode} | Status: ${statusText}`);
-    super(message);
-      this.name = "FetchFailedError";
-      this.statusCode = statusCode;
-      this.statusText = statusText;
+        super(message);
+        this.name = "FetchFailedError";
     }
 }
