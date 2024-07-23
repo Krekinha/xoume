b9a821dc5d679ea99b2c815f8c95f6d3c90206f6
Krekinha <krekmg@gmail.com>
2024-07-23 05:25:16 -0300

    05:25

--- src/utils/dataFaker.ts
+++ src/utils/dataFaker.ts
@@ -1,13 +1,12 @@
 import { fakerPT_BR } from "@faker-js/faker";
 import type { Transporte } from "./types";
 
-export const empresas = Array.from({ length: 10 }).map(() => {
+export const empresas = Array.from({ length: 10 }).map(() => ({
 	razaoNome: fakerPT_BR.helpers.arrayElements([
 		"TRANSPORTADORA F & R",
 		"EXPRESSO DIESEL TRANSPORTES",
 	]),
-    
-});
+}));
 
 export const transportes = Array.from({ length: 10 }).map(() => {
 	//id: fakerPT_BR.number.int({ min: 1, max: 10 });
