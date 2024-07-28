2678e3813f78933bd9bb6f9835827a023a1fc3f8
Krekinha <krekmg@gmail.com>
2024-07-27 22:19:46 -0300

    bug-theme retirado colors de tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
-import colors from "tailwindcss/colors";
-
 module.exports = {
 	content: [
 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
 	],
-	theme: {
-		extend: {
-			colors: colors,
-		},
-	},
+	theme: {},
 	plugins: [require("flowbite/plugin")],
 	darkmode: "class",
 };
